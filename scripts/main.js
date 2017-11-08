ecc6eebba2eaf4b305bc52a3c94331092f65164d
chad hughes <chadders64@hotmail.com>
2017-11-08 18:09:12 -0500

    Got the other page to work

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 
 $( "#newTastingbutton" ) .click(function() {
-    $("#mainBody img, #newTasting").fadeTo("slow", 0.2, function(){
+    $("#mainBody img, #tForm ").fadeTo("slow", 0.2, function(){
         $( "#mainBody h1, #newTasting h1").fadeIn("2600", 1.0)
     });
   });
