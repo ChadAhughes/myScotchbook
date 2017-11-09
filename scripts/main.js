dacd672d4e57b0bf9ce28ef23aa030977826d0f9
chad hughes <chadders64@hotmail.com>
2017-11-08 19:52:44 -0500

    i think i am finished with the requirments

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@
 
 $( "#newTastingbutton" ) .click(function() {
     $("#mainBody img, #tForm ").fadeTo("slow", 0.2, function(){
-        $( "#mainBody h1, #newTasting h1").fadeIn("2600", 1.0)
-    });
+        $( "#mainBody h1, #newTasting h1").fadeIn("2600", 1.0) });
   });
+  
+  $( document ).ready(function() {
+    $( "#Whiskey-name" ).focus();
+  });
+
