7dfd2ce70d66927c0011d556959c971b94dcc2dc
CHAD HUGHES <chadders64@hotmail.com>
2017-11-09 18:04:20 -0500

    hope this works

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 
 $( "#newTastingbutton" ) .click(function() {
     $("#mainBody img, #tForm ").fadeTo("slow", 0.2, function(){
-        $( "#mainBody h1, #newTasting h1").fadeIn("2600", 1.0) });
+        $( "#mainBody h1, #newTasting h1").fadeIn(, 1.0) });
   });
   
   $( document ).ready(function() {
